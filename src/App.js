9ad1df7d2152a4872681ed5a60be2492bd0fb489
wlduddnjs789 <wlduddnjs789@gmail.com>
2023-03-02 16:54:15 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponet from "./main";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponet />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponet />
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
